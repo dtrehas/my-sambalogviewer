8891dd44fcc1b33d0178a8af36476ef1ab5834eb
stefan.ponikau@googlemail.com <stefan.ponikau@googlemail.com@1130991f-62d6-ae9f-c1a5-cc89e26c7520>
2011-05-21 20:42:38 +0000

    beispel für ein branche

---
+++ branches/branche1/svn/trunk/main/Main.java
@@ -0,0 +1,34 @@
+package main;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+import java.io.File;
+import org.eclipse.swt.*;
+import gui.Fenster1;
+
+/**
+ *@version 1.1
+ * @author stefan
+ */
+
+public class Main {
+
+    /**
+     * Main  startet die Dateisuche und läd das Ergebnis in das Fenster.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File f = new File("/sambalogs");
+        dateiauslesen dl = new dateiauslesen(f);
+
+        Fenster1 window1 = new Fenster1(dl.files);
+
+
+        // TODO code application logic here
+    }
+}
